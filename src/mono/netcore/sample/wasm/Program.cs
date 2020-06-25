0903159e73f115ebc2b1d9209ba5f2a7c9f9666d
Katelyn Gadd <kg@luminance.org>
2020-06-24 20:11:34 -0700

    Fix typo

--- src/mono/netcore/sample/wasm/Program.cs
+++ src/mono/netcore/sample/wasm/Program.cs
@@ -9,6 +9,6 @@ public class Test
     public static void Main (String[] args) {
         Console.WriteLine ("Hello, World!");
         var now = DateTime.Now;
-        Console.WriteLine ("Now: ", now);
+        Console.WriteLine ("Now: {0}", now);
     }
 }
