26c9fdcf0234d05381af41d38c7d73b1854f976a
Katelyn Gadd <kg@luminance.org>
2025-03-25 14:36:12 -0700

    Forward System.GC.Collect calls in interpreter to a dedicated opcode that invokes the GC so we can test interpreter stack walking

--- src/coreclr/vm/interpexec.cpp
+++ src/coreclr/vm/interpexec.cpp
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 #include "interpexec.h"
+#include "threads.h"
+#include "gcenv.h"
 #include <math.h>
 
 #ifdef FEATURE_INTERPRETER
@@ -735,6 +737,13 @@ MAIN_LOOP:
                 stack = pFrame->pStack;
                 ip = pFrame->startIp + sizeof(InterpMethod*) / sizeof(int32_t);
                 pThreadContext->pStackPointer = stack + pMethod->allocaSize;
+                break;
+            }
+            case INTOP_COLLECT: {
+                // HACK: blocking gc of all generations
+                // FIXME: currently hangs and stack walk does not ever reach the interpreter code manager
+                GCX_COOP();
+                GCHeapUtilities::GetGCHeap()->GarbageCollect(-1, false, 0x00000002);
                 break;
             }
             case INTOP_FAILFAST:
